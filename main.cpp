3620428c6402ea8bb421aa4bc248e22934a2a223
Vitaly Davydov <vitprof@gmail.com>
2021-08-11 09:52:54 +0100

    Added Qt project file (.pro)

--- main.cpp
+++ main.cpp
@@ -1,1 +1,10 @@
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QWidget>
 
+int main(int argc, char* argv[])
+{
+    QApplication app(argc, argv);
+    QWidget w;
+    w.show();
+    return app.exec();
+}
